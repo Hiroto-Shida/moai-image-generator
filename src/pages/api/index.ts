96131ec8303fb6043e6914d63c7830c7c1f943bf
Hiroto-Shida <uecshida6295@gmail.com>
2024-11-25 00:02:28 +0900

    cacheの設定を追加

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -13,12 +13,14 @@ export default async function handler(req: NextRequest) {
   const title = url.searchParams.get("title") || "LGTM";
   const subTitle = url.searchParams.get("subTitle") || "Looks Good To Me";
 
-  const num = Math.floor(Math.random() * 8) + 1;
+  // 現在の時刻を用いて、1-8までのランダムな数値を生成
+  const timestamp = new Date().getTime(); // 現在の時間をミリ秒単位で取得
+  const num = (timestamp % 8) + 1;
   const imagePath = `${process.env.NEXT_PUBLIC_VERCEL_URL}/images/${num}.png`;
 
   const size = 400;
 
-  return new ImageResponse(
+  const response = new ImageResponse(
     React.createElement(OgpComponent, { title, subTitle, imagePath, size }),
     {
       width: size,
@@ -33,4 +35,15 @@ export default async function handler(req: NextRequest) {
       ],
     }
   );
+
+  // cacheの無効化設定
+  // MEMO: ブラウザでキャッシュしている場合はこれでも対応不可能
+  response.headers.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  response.headers.set("Expires", "0");
+  response.headers.set("Pragma", "no-cache");
+
+  return response;
 }
