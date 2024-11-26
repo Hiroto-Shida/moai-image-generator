858269c16e0be541e87c8526786653677054556a
Hiroto-Shida <uecshida6295@gmail.com>
2024-11-26 22:24:53 +0900

    URLクエリの操作を整理

--- src/components/Top/index.tsx
+++ src/components/Top/index.tsx
@@ -11,9 +11,12 @@ export type FormType = {
 
 type Props = {
   pageUrl: string;
+  image: string;
+  main?: string;
+  sub?: string;
 };
 
-const Top: React.FC<Props> = ({ pageUrl }) => {
+const Top: React.FC<Props> = ({ pageUrl, image, main, sub }) => {
   const [imageSrc, setImageSrc] = useState<string | null>(null);
   const [isOpenToast, setIsOpenToast] = useState<boolean>(false);
   const [generatedUrl, setGeneratedUrl] = useState<string>("");
@@ -22,34 +25,36 @@ const Top: React.FC<Props> = ({ pageUrl }) => {
   const methods = useForm<FormType>({
     mode: "onChange",
     defaultValues: {
-      image: "happy",
-      main: "LGTM",
-      sub: "Looks Good To Me",
+      image: image,
+      main: main || "LGTM",
+      sub: sub || "Looks Good To Me",
     },
   });
 
   const fetchImage = useCallback(
     async (image: string, main: string, sub: string) => {
       const imageName = imagesList.includes(image) ? image : "happy";
-      const topUrl =
-        `?image=${imageName}` +
-        `${main || sub ? "&" : ""}` +
-        `${main ? `main=${main}` : ""}` +
-        `${main && sub ? "&" : ""}` +
-        `${sub ? `sub=${sub}` : ""}`;
-      const apiUrl =
-        `/api/${imageName}` +
-        `${main || sub ? "?" : ""}` +
-        `${main ? `main=${main}` : ""}` +
-        `${main && sub ? "&" : ""}` +
-        `${sub ? `sub=${sub}` : ""}`;
+
+      const queryObj: { main?: string; sub?: string } = {};
+      if (main) queryObj.main = main;
+      if (sub) queryObj.sub = sub;
+      const queryStr = new URLSearchParams(queryObj).toString();
+
+      const topUrl = queryStr
+        ? `?image=${imageName}&${queryStr}`
+        : `?image=${imageName}`;
+
+      const apiUrl = queryStr
+        ? `/api/${imageName}?${queryStr}`
+        : `/api/${imageName}`;
+
       try {
-        const response = await fetch(apiUrl.replace(/\s/g, "%20"));
+        const response = await fetch(apiUrl);
         const blob = await response.blob();
         const imageUrl = URL.createObjectURL(blob);
         setImageSrc(imageUrl);
-        setGeneratedUrl(`${pageUrl}${topUrl.replace(/\s/g, "%20")}`);
-        setGeneratedGithubUrl(`![](${pageUrl}${apiUrl.replace(/\s/g, "%20")})`);
+        setGeneratedUrl(`${pageUrl}${topUrl}`);
+        setGeneratedGithubUrl(`![](${pageUrl}${apiUrl})`);
       } catch (error) {
         console.error("Error fetching image:", error);
       }
@@ -58,8 +63,8 @@ const Top: React.FC<Props> = ({ pageUrl }) => {
   );
 
   useEffect(() => {
-    fetchImage("", "", "");
-  }, [fetchImage]);
+    fetchImage(image, main || "", sub || "");
+  }, [fetchImage, image, main, sub]);
 
   const onSubmit: SubmitHandler<FormType> = (data) => {
     fetchImage(data.image, data.main, data.sub);
