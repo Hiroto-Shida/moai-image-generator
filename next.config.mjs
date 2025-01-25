62f0b1a795dc0173d3439641d386344356c1eb91
Hiroto-Shida <uecshida6295@gmail.com>
2025-01-25 18:29:09 +0900

    :rocket: dependencies updateに伴って設定周りの修正、cspの設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,29 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
+const securityHeaders = [
+  {
+    // HTTP の代わりに HTTPS を用いて通信を行うよう指示する
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    // XSS対策
+    key: "Content-Security-Policy",
+    value: cspHeader.replace(/\n/g, ""),
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -17,11 +43,15 @@ const nextConfig = {
     });
     return config;
   },
-  // images: {
-  //   // svgへの型付けのため画像に関するデフォルトの型定義を無効化
-  //   // https://zenn.dev/catnose99/articles/49c12f84182bdf
-  //   disableStaticImages: true,
-  // },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
 
   compiler: {
     styledComponents: true,
